55343124e3da0aa26a4e7aa26c7812b723ae50e6
celebrated-ninja-3247 <cnunlee7013@gmail.com>
2019-12-14 17:04:32 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,3 @@
 # Define the variable board below.
-row = [ " ", " ", " "]
+row = [ " ", " ", " ", " ", " ", " ", " "," ", " "]
 
-# position 1: Left
-row[0]
-
-#position 2: Middle
-row[2]
-
-# Move x to position 2,
-Middle
-row[1] = "x"
-row #=> [" ", "X", "."]
